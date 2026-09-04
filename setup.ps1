e833a1900b779b30e6e7e92d60a7de65351570ca
Andrii <damvyn@users.noreply.github.com>
2026-09-04 23:41:51 +0300

    add version detection

--- setup.ps1
+++ setup.ps1
@@ -42,5 +42,35 @@ function Download-File {
 }
 
 
+$TargetDir = Read-Host -Prompt "Enter ProjectTarget Directory. Press Enter for C:\PowPuLa"
+If ([string]::IsNullOrEmpty($TargetDir)) { $TargetDir = "C:\PowPuLa" }
+$null = New-Item -Path $TargetDir -ItemType Directory -Force
+
+
+$PyVer = Read-Host -Prompt "Enter Python Version. Press Enter for latest"
+If ([string]::IsNullOrEmpty($PyVer)) { $PyVer = "Latest" }
+
+#region download Python
+# get Python versions from the official Python api
+$BaseUrl = 'https://www.python.org/api/v2/downloads/release/?is_published=true'
+
+$VersUrl = if ($PyVer -eq 'Latest') { 
+    $BaseUrl + '&is_latest=true&pre_release=false&version=3'
+} else { $BaseUrl + "&pre_release=false&name=Python+$PyVer" }
+
+Write-Host "Fetching version information from $VersUrl"
+$versionData = Invoke-RestMethod -Uri $VersUrl -UseBasicParsing -DisableKeepAlive -ErrorAction Stop
+
+# detect the desired version is present
+$WantedVersion = $versionData |
+    Sort-Object {$_.name.Replace("Python ", "") -as [version]} |
+    Select-Object -Last 1
+
+If (-not $WantedVersion) {
+    Write-Host "No matching Python version found for '$PyVer'." -ForegroundColor Red
+    return
+} else {Write-Host "Found version $($WantedVersion.name)"}
+#endregion download Python
+
 
 
